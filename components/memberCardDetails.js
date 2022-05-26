9db67057c98e234afdd6643ef36080896d6da350
rushilshakya <rushil.shakya@gmail.com>
2022-05-26 09:23:40 +0545

    feat: Member card with bio first cut

---
+++ components/memberCardDetails.js
@@ -0,0 +1,21 @@
+export default function MemberCardDetails({ member }) {
+  return (
+    <div className="card">
+      <div className="card-header">
+        <div className="card_img">
+          <img src={member.photo} />
+        </div>
+        <div className="card_title">
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href={member.linkedIn}
+            className="body1"
+          >
+            {member.name}
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
