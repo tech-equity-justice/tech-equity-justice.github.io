3262ab5a3ee95e77abf920bafeaad5d9705dbfd8
nepsoul <nirumagar901@gmail.com>
2024-04-07 21:21:34 +0545

    chore: add success-stories scss

--- components/storyCardDetails.js
+++ components/storyCardDetails.js
@@ -2,26 +2,24 @@ import Image from "next/image";
 
 export default function StoryCardDetails({ member }) {
   return (
-    <div>
-      <div>
-        <div>
-          <Image src={member.photo} alt="" width={50} height={50} />
-        </div>
-        <div>
-          <div>{member.name}</div>
-          <div>{member.title}</div>
-          <div>{member.company}</div>
-          <div>
-            <Image src={member.logo} alt="" width={90} height={40} />
-          </div>
-        </div>
-        <div>
+    <div className="success_card">
+      <div className="alumni_card">
+        <div className="alumni_image">
           <Image
-            src="/images/logos/TEJ-main-lg.png"
-            alt="TEJ Fellowship logo"
-            width={50}
-            height={50}
+            src={member.photo}
+            alt={member.name}
+            width={100}
+            height={100}
           />
+        </div>
+        <div className="alumni_info">
+          <div className="info_font">
+            <h3>{member.name}</h3>
+            <p>{member.title}</p>
+          </div>
+          <div className="company_logo">
+            <Image src={member.logo} alt={member.logo} width={90} height={50} />
+          </div>
         </div>
       </div>
       <div>{member.content}</div>
