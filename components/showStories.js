8597a742c0d8a4ba86a14b87c531fbd8302a9895
rushilshakya <rushil.shakya@gmail.com>
2024-05-27 12:01:12 +0545

    chore: refactor to conventions

--- components/showStories.js
+++ components/showStories.js
@@ -2,10 +2,10 @@ import StoryCardDetails from "./storyCardDetails";
 export default function ShowStories({ members, title, tagID }) {
   return (
     <>
-      <div className="backgroundColor">
+      <div className="pink-block">
         <div className="wrapper">
           <div id={tagID && tagID}>
-            <p className="h2 title">{title}</p>
+            <p className="h2 align-center primary-color region-md">{title}</p>
           </div>
           <div className="card-wrapper">
             {members.map((member) => (
