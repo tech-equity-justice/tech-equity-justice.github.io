eff37e4e0855020220d38e3200edab6b6db7b048
Nepsoul <nirumagar901@gmail.com>
2024-06-17 02:07:06 +0545

    chore: add features into testimony modal

--- components/testimonyModal.js
+++ components/testimonyModal.js
@@ -7,55 +7,69 @@ export default function TestimonyModal({ member, onClose }) {
     }
   };
   return (
-    <div className="flex flex-center testimony-modal" onClick={handleClick}>
+    <div className="testimony-modal flex flex-center" onClick={handleClick}>
       <div
         className="testimony-modal-card success_card"
         onClick={(e) => e.stopPropagation()}
       >
-        <button className="testimony-modal_close" onClick={onClose}>
-          &times;
-        </button>
-        <div className="alumni_card grid-template-testimony">
-          <div className="alumni_image">
-            <Image
-              src={member.photo}
-              alt={member.name}
-              width={100}
-              height={100}
-            />
+        <div className="flex flex-direction-sm space-between">
+          <div className="flex flex-column-testimony flex-start-sm">
+            <div className="modal-image">
+              <Image
+                src={member.photo}
+                alt={member.name}
+                width={100}
+                height={100}
+              />
+            </div>
+            <div className="testimony-info">
+              <div className="info_font">
+                {member.linkedIn !== "" ? (
+                  <a target="_blank" rel="noreferrer" href={member.linkedIn}>
+                    <h3>{member.name}</h3>
+                  </a>
+                ) : (
+                  <a>
+                    <h3>{member.name}</h3>
+                  </a>
+                )}
+                <p>{member.title}</p>
+              </div>
+              <div className="company_logo">
+                <a target="_blank" href={member.company_link}>
+                  <Image
+                    src={member.logo}
+                    alt={member.logo}
+                    width={120}
+                    height={40}
+                  />
+                </a>
+              </div>
+            </div>
           </div>
-          <div className="alumni_info">
-            <div className="info_font">
-              {member.linkedIn !== "" ? (
-                <a target="_blank" rel="noreferrer" href={member.linkedIn}>
-                  <h3>{member.name}</h3>
-                </a>
-              ) : (
-                <a>
-                  <h3>{member.name}</h3>
-                </a>
-              )}
-              <p>{member.title}</p>
-            </div>
-            <div className="company_logo">
-              <a target="_blank" href={member.company_link}>
-                <Image
-                  src={member.logo}
-                  alt={member.logo}
-                  width={120}
-                  height={40}
-                />
-              </a>
-            </div>
+          <div className="testimony-modal_close">
+            <span className="flex close-button" onClick={onClose}>
+              &times;
+            </span>
           </div>
         </div>
-        {member.content !== "" && (
-          <>
-            <p className="alumni_content">
-              <span dangerouslySetInnerHTML={{ __html: member.content }} />
-            </p>
-          </>
-        )}
+        <div className="flex">
+          <div className="quote-width">
+            <Image
+              src={"/images/quote/quote.svg"}
+              width={25}
+              height={25}
+              alt="quote"
+            />
+          </div>
+          {member.content !== "" && (
+            <>
+              <p className="testimony-content">
+                <span dangerouslySetInnerHTML={{ __html: member.content }} />
+              </p>
+            </>
+          )}
+        </div>
         <div className="content_detail margin-top-testimony"></div>
       </div>
     </div>
